340c1dd8007e17fbba612639d4416b4cf775cbb0
Adrian <adrian.garciaprado@nrg.com>
2017-10-25 13:23:27 -0500

    Add automated installer

---
+++ scripts/configure.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set -e
+
+CONFIG_DIR=$1
+
+link (){
+  source=$1
+  destination=$2
+  echo "# linkt ${source} to ${destination}"
+  ln -sf $source $destination
+}
+
+echo "# Define files that need to be backed-up and linked:"
+echo "#   - bashrc "
+echo "#   - bash_profile "
+echo "#   - bash/ "
+{
+  case "$SHELL" in
+  /bin/bash )
+    profile="${HOME}/.bash_profile"
+    bashrc="${HOME}/.bashrc"
+    bash="${HOME}/.bash"
+    ;;
+  * )
+    profile="your profile"
+    bashrc="your bashrc"
+    bash="your bash"
+    ;;
+  esac
+} >&2
+
+
+echo "# Move the original files to *_orig name."
+if [ -f "${profile}" ] && [ ! -f "${profile}_orig" ]; then
+  echo "# Move old ${profile} to ${profile}_orig backup"
+  mv $profile "${profile}_orig"
+fi
+
+if [ -f "${bashrc}" ] && [ ! -f "${bashrc}_orig" ]; then
+  echo "# Move old ${bashrc} to ${bashrc}_orig backup"
+  mv $bashrc "${bashrc}_orig"
+fi
+
+if [ -d "${bash}" ] && [ ! -d "${bash}_orig" ]; then
+  echo "# Move old ${bash} to ${bash}_orig backup"
+  mv $bash_folder "${bash_folder}_orig"
+fi
+
+link $CONFIG_DIR/bashrc        $bashrc
+link $CONFIG_DIR/bash_profile  $profile
+link $CONFIG_DIR/bash          $bash
---
+++ scripts/installer.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -z "$UNIX_CONFIG_PATH" ]; then
+  UNIX_CONFIG_PATH="${HOME}/.unix_configuration_files"
+fi
+echo "Define Unix Configuration Files folder in ${UNIX_CONFIG_PATH}"
+
+if [ ! -d "$UNIX_CONFIG_PATH" ]; then
+  mkdir ${UNIX_CONFIG_PATH}
+  echo "Create Unix Configuration Files folder: ${UNIX_CONFIG_PATH}"
+fi
+
+checkout() {
+  echo "Clone git repository ${1}"
+  [ -d "$2" ] || git clone --recursive "$1" "$2"
+}
+
+configure() {
+  sh $1/scripts/configure.sh $1
+}
+
+if ! command -v git 1>/dev/null 2>&1; then
+  echo "git: Git is not installed, can't continue." >&2
+  exit 1
+fi
+
+if [ -n "${USE_GIT_URI}" ]; then
+  GITHUB="git://github.com"
+else
+  GITHUB="https://github.com"
+fi
+
+repository="${GITHUB}/AdrianGPrado/bash-configuration.git"
+destination="${UNIX_CONFIG_PATH}/bash-configuration"
+
+checkout $repository $destination
+configure $destination
